01e4ec73900c36da779b9ff62fa946972168243d
Carter Parrett <cparrett94@hotmail.com>
2020-07-09 01:46:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,11 @@ def oxford_comma(array)
   if array.length >= 2
     num = array[-1]
     num = "and #{num}"
-    
+    array.pop
+    num.push (num)
+    array.join(", ")
+  else
+    array.join (" and ")
+  end
 end
 
